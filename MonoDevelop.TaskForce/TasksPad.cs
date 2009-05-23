dbef53ec8a83de861cb018b92eebb2fb9a91eff7
Anirudh Sanjeev <andy@ninjagod.com>
2009-05-23 18:17:29 +0530

    addin's not working yet :(

---
+++ MonoDevelop.TaskForce/TasksPad.cs
@@ -0,0 +1,24 @@
+using System;
+using MonoDevelop.Ide.Gui;
+using Gtk;
+namespace MonoDevelop.TaskForce
+{
+	class TasksPad : AbstractPadContent
+	{
+		public TasksPad()
+		{
+			
+			
+		}
+		
+		public override Gtk.Widget Control
+		{
+			get
+			{
+				Gtk.Label temp = new Gtk.Label();
+				temp.Text = "Hello World";
+				return temp;
+			}
+		}
+	}
+}
